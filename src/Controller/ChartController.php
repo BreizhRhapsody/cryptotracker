d2fdd1f11a363883ae6fd8f9e0fb24f0b7f68a6b
BreizhRhapsody <manonp2278@gmail.com>
2022-05-09 13:39:28 +0200

    Update ChartController

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -3,16 +3,41 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\SaveOfJourney;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ChartController extends AbstractController
 {
     #[Route('/chart', name: 'app_chart')]
-    public function index(): Response
+
+    public function index(ChartBuilderInterface $chartBuilder, ManagerRegistry $doctrine): Response
     {
+        $repository = $doctrine->getRepository(SaveOfJourney::class);
+        $dailyProfits = $repository->findAll();
+
+        $horizontal = [];
+        $vertical = [];
+        foreach ($dailyProfits as $dailyProfit) {
+            array_push($horizontal, $dailyProfit->getDate()->format('d/m/Y'));
+            array_push($vertical, $dailyProfit->getProfit());
+        }
+
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => $horizontal,
+            'datasets' => [
+                'label' => 'Evolution',
+                'borderColor' => '#1fc36c',
+                'data' => $vertical,
+            ]
+        ]);
+
         return $this->render('chart/index.html.twig', [
-            'controller_name' => 'ChartController',
+            'chart' => $chart,
         ]);
     }
 }
